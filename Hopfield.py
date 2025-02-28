10654810d42edca5ed9f4b9980f6fa4fb1c64181
RenMortem <gumballo1@outlook.com>
2025-02-27 23:18:09 -0600

    fix error b igual a cero
    Cuando b era igual a cera, la función sign() quedaba indefinida. Se usó la condicional if para agregar un caso donde no se indefiniera.

--- Hopfield.py
+++ Hopfield.py
@@ -3,8 +3,9 @@ import matplotlib.pyplot as plt
 
 x1 = np.array([[1], [-1], [-1], [1]])
 x1_mod = np.array([[1], [-1], [-1], [-1]])
+x2_mod = np.array([[1], [1], [1], [-1]]) #Si  la más de la mitad de los pixeles están distorcionados, la imagen restaruada se invierte, para arreglarlo, hay que experimentar con valores de "u".
 x1_T = x1.T
-u = 0
+u = 0.25
 b = np.zeros(4)
 x2 = np.zeros(4, dtype=int)
 
@@ -12,12 +13,14 @@ W1 = (1 / len(x1)) * (x1 @ x1.T)
 
 for i in range(4):
     for j in range(4):
-        b[i] += W1[i, j] * x1_mod[j, 0] - u
+        b[i] += W1[i, j] * x2_mod[j, 0] - u
+        if b[i] == 0:
+            b[i] = 1
     x2[i] = np.sign(b[i])
     
 # Convertir a forma 2x2
 imagen1 = x1.flatten().reshape(2, 2)
-imagen2 = x1_mod.flatten().reshape(2, 2)
+imagen2 = x2_mod.flatten().reshape(2, 2)
 imagen3 = x2.flatten().reshape(2, 2)
 
 # Mostrar ambas imágenes
@@ -38,8 +41,8 @@ plt.imshow(imagen3, cmap='gray', vmin=-1, vmax=1)
 plt.title("Imagen Restaurada")
 plt.axis('off')
 
-plt.show()
-
 print(W1)
 print(b)
 print(x2)
+
+plt.show()
